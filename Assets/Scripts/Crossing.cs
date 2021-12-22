105dd7e39cbb1fbd5aadb310f38c6691b2d29418
Alejandro <alexhapki@hotmail.com>
2021-12-22 15:27:31 +0100

    First commit First commit ºº

---
+++ Assets/Scripts/Crossing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Crossing : MonoBehaviour
+{
+
+    public List<GameObject> pedestrianList = new List<GameObject>();
+    public bool pedestrianCrossing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "NPC")
+        {
+            pedestrianList.Add(col.gameObject);
+            pedestrianCrossing = true;
+      //      Debug.Log("Pedestrian");
+        }
+    }
+
+    private void OnTriggerExit (Collider col)
+    {
+        if (col.tag == "NPC")
+        {
+            pedestrianList.Remove (col.gameObject);
+      //      Debug.Log(pedestrianList.Count);
+            if (pedestrianList.Count == 0)
+            {
+                pedestrianCrossing = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/JunctionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunctionController : MonoBehaviour
+{
+
+    public List<GameObject> higherPriority = new List<GameObject>();
+    public List<GameObject> vehicleList = new List<GameObject>();
+    private Transform stoppingSpot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
